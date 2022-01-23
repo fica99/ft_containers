31938119eab5dca79aa15f347a8ff9960b0f6f39
Filip Crnobrnja <fica.c@yandex.ru>
2022-01-23 16:32:05 +0100

    [ft][iterator] Move iterator base class to iterator file

--- ft/iterator/iterator.hpp
+++ ft/iterator/iterator.hpp
@@ -6,16 +6,35 @@
 /*   By: aashara- <aashara-@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 20:46:01 by aashara-          #+#    #+#             */
-/*   Updated: 2022/01/23 12:19:54 by aashara-         ###   ########.fr       */
+/*   Updated: 2022/01/23 16:33:04 by aashara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITERATOR_HPP
 # define ITERATOR_HPP
 
+#include <cstddef>
+
 #include "iterator_traits.hpp"
-#include "iterator_base.hpp"
 #include "reverse_iterator.hpp"
 
+namespace ft
+{
+
+template<
+    class Category,
+    class T,
+    class Distance = std::ptrdiff_t,
+    class Pointer = T*,
+    class Reference = T&
+> struct iterator
+{
+    typedef Category iterator_category;
+    typedef T value_type;
+    typedef Distance difference_type;
+    typedef Pointer pointer;
+    typedef Reference reference;
+};
+}
 
 #endif // ITERATOR_HPP
