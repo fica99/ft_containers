4bae2dca864d009e7f378e34c8c6c7a075563aad
Filip Crnobrnja <fica.c@yandex.ru>
2022-01-23 16:34:52 +0100

    [ft][utils][type_traits] Add is_integral
    * Add is_integral and parent classes is_integral_type and is_integral_base

--- ft/utils/type_traits.hpp
+++ ft/utils/type_traits.hpp
@@ -6,12 +6,14 @@
 /*   By: aashara- <aashara-@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 13:09:33 by aashara-          #+#    #+#             */
-/*   Updated: 2022/01/23 15:54:48 by aashara-         ###   ########.fr       */
+/*   Updated: 2022/01/23 16:28:39 by aashara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPE_TRAITS_HPP
 # define TYPE_TRAITS_HPP
+
+#include <uchar.h>
 
 namespace ft
 {
@@ -21,6 +23,101 @@ struct enable_if {};
  
 template<class T>
 struct enable_if<true, T> { typedef T type; };
+
+template <bool is_integral, typename T>
+struct is_integral_base
+{
+// Member types
+    typedef T type;
+    typedef bool value_type;
+// Member constants
+    static const value_type value = is_integral;
+// Member functions
+    operator bool() const
+    {
+        return value;
+    }
+};
+
+template <typename>
+struct is_integral_type : public is_integral_base<false, bool>
+{
+};
+
+template <>
+struct is_integral_type<bool> : public is_integral_base<true, bool>
+{
+};
+
+template <>
+struct is_integral_type<char> : public is_integral_base<true, char>
+{
+};
+
+template <>
+struct is_integral_type<unsigned char> : public is_integral_base<true, unsigned char>
+{
+};
+
+template <>
+struct is_integral_type<char16_t> : public is_integral_base<true, char16_t>
+{
+};
+
+template <>
+struct is_integral_type<char32_t> : public is_integral_base<true, char32_t>
+{
+};
+
+template <>
+struct is_integral_type<wchar_t> : public is_integral_base<true, wchar_t>
+{
+};
+
+template <>
+struct is_integral_type<short> : public is_integral_base<true, short>
+{
+};
+
+template <>
+struct is_integral_type<unsigned short> : public is_integral_base<true, unsigned short>
+{
+};
+
+template <>
+struct is_integral_type<int> : public is_integral_base<true, int>
+{
+};
+
+template <>
+struct is_integral_type<unsigned int> : public is_integral_base<true, unsigned int>
+{
+};
+
+template <>
+struct is_integral_type<long> : public is_integral_base<true, long>
+{
+};
+
+template <>
+struct is_integral_type<unsigned long> : public is_integral_base<true, unsigned long>
+{
+};
+
+template <>
+struct is_integral_type<long long> : public is_integral_base<true, long long>
+{
+};
+
+template <>
+struct is_integral_type<unsigned long long> : public is_integral_base<true, unsigned long long>
+{
+};
+
+template <typename T>
+struct is_integral : public is_integral_type<T>
+{
+};
 
 }
 #endif // TYPE_TRAITS_HPP
